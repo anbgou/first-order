0560cc892fd19f2841001e29413d2df0206567f9
anbgou <gerdunjapes@gmail.com>
2024-04-08 20:13:51 +0300

    Added js, done everything except adaptation

---
+++ script.js
@@ -0,0 +1,25 @@
+const telegram = document.querySelector(".telegram");
+const instagram = document.querySelector(".instagram");
+const discord = document.querySelector(".discord");
+const tiktok = document.querySelector(".tiktok");
+const youtube = document.querySelector(".youtube");
+
+telegram.addEventListener("click", function () {
+    window.open("https://web.telegram.org/k/#@Slashu", "_blank");
+})
+
+instagram.addEventListener("click", function () {
+    window.open("https://www.instagram.com/romoto9/", "_blank");
+})
+
+discord.addEventListener("click", function () {
+    window.open("discordapp.com/users/408166941006626816", "_blank");
+})
+
+tiktok.addEventListener("click", function () {
+    window.open("https://www.tiktok.com/@romoto_g", "_blank");
+})
+
+youtube.addEventListener("click", function () {
+    window.open("https://youtube.com/@volodyarospopa167?si=Wmgzm5_bzDbfHbpx", "_blank");
+})
